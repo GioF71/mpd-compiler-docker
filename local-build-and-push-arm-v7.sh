fb79cea4c22e3051fbddb6448943fc0f9978849f
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-06-15 11:12:56 +0200

    Add pre-built binaries

---
+++ local-build-and-push-arm-v7.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TODAY=$(date '+%Y-%m-%d')
+MPD_VERSION=0.24.4
+
+echo "TODAY=${TODAY}"
+
+# debian bookworm
+docker buildx build . \
+    --platform linux/arm/v5 \
+    --build-arg BASE_IMAGE=debian:bookworm-slim \
+    --tag giof71/mpd-compiler:bookworm-armv7-${MPD_VERSION}-${TODAY} \
+    --tag giof71/mpd-compiler:bookworm-armv7-${MPD_VERSION} \
+    --tag giof71/mpd-compiler:bookworm-armv7 \
+    --tag giof71/mpd-compiler:latest-armv7 \
+    --tag giof71/mpd-compiler:stable-armv7 \
+    --load \
+    --push

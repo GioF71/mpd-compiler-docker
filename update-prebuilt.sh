6e5fb9d7cba89012e1af1584dc60724a7a4ee35b
Giovanni Fulco <giovanni.fulco@gmail.com>
2025-06-15 11:49:28 +0200

    Avoid redundant directory structure

--- update-prebuilt.sh
+++ update-prebuilt.sh
@@ -12,13 +12,13 @@ docker create --name mpd-v6 giof71/mpd-compiler:latest-armv6
 docker create --name mpd-v7 giof71/mpd-compiler:latest-armv7
 docker create --name mpd-v8 giof71/mpd-compiler:latest-armv8
 docker create --name mpd-amd64 giof71/mpd-compiler:latest-amd64
-docker cp mpd-v5:/app/bin/compiled/mpd assets/precompiled/arm/v5/mpd.${MPD_VERSION}.v5
-docker cp mpd-v5:/app/bin/compiled/mpd-ups assets/precompiled/arm/v5/mpd.${MPD_VERSION}-ups.v5
-docker cp mpd-v6:/app/bin/compiled/mpd assets/precompiled/arm/v6/mpd.${MPD_VERSION}.v6
-docker cp mpd-v6:/app/bin/compiled/mpd-ups assets/precompiled/arm/v6/mpd.${MPD_VERSION}-ups.v6
-docker cp mpd-v7:/app/bin/compiled/mpd assets/precompiled/arm/v7/mpd.${MPD_VERSION}.v7
-docker cp mpd-v7:/app/bin/compiled/mpd-ups assets/precompiled/arm/v7/mpd.${MPD_VERSION}-ups.v7
-docker cp mpd-v8:/app/bin/compiled/mpd assets/precompiled/arm/v8/mpd.${MPD_VERSION}.v8
-docker cp mpd-v8:/app/bin/compiled/mpd-ups assets/precompiled/arm/v8/mpd.${MPD_VERSION}-ups.v8
-docker cp mpd-amd64:/app/bin/compiled/mpd assets/precompiled/amd/64/mpd.${MPD_VERSION}.amd64
-docker cp mpd-amd64:/app/bin/compiled/mpd-ups assets/precompiled/amd/64/mpd.${MPD_VERSION}-ups.amd64
+docker cp mpd-v5:/app/bin/compiled/mpd assets/precompiled/mpd.${MPD_VERSION}.arm.v5
+docker cp mpd-v5:/app/bin/compiled/mpd-ups assets/precompiled/mpd.${MPD_VERSION}-ups.arm.v5
+docker cp mpd-v6:/app/bin/compiled/mpd assets/precompiled/mpd.${MPD_VERSION}.arm.v6
+docker cp mpd-v6:/app/bin/compiled/mpd-ups assets/precompiled/mpd.${MPD_VERSION}-ups.arm.v6
+docker cp mpd-v7:/app/bin/compiled/mpd assets/precompiled/mpd.${MPD_VERSION}.arm.v7
+docker cp mpd-v7:/app/bin/compiled/mpd-ups assets/precompiled/mpd.${MPD_VERSION}-ups.arm.v7
+docker cp mpd-v8:/app/bin/compiled/mpd assets/precompiled/mpd.${MPD_VERSION}.arm.v8
+docker cp mpd-v8:/app/bin/compiled/mpd-ups assets/precompiled/mpd.${MPD_VERSION}-ups.arm.v8
+docker cp mpd-amd64:/app/bin/compiled/mpd assets/precompiled/mpd.${MPD_VERSION}.amd64
+docker cp mpd-amd64:/app/bin/compiled/mpd-ups assets/precompiled/mpd.${MPD_VERSION}-ups.amd64
